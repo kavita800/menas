7227a3535493a341e6c33ebcacf5f7e457681206
kavita800 <kavitabhakuni800@gmail.com>
2020-08-24 13:10:14 +0530

    location

--- src/app/shared/config/address.config.ts
+++ src/app/shared/config/address.config.ts
@@ -6,7 +6,7 @@ export const AddressTabConfig =  [
         "number" :"(708) 474-4522",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " 24 hours, 7 days a week"
     },
     {
@@ -16,7 +16,7 @@ export const AddressTabConfig =  [
         "number" :"(708) 799-4065",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex":"(708) 679-1910",
+        "fax":"(708) 679-1910",
         "time" : " 24 hours, 7 days a week"
     },{
         "id" : "matteson",
@@ -25,7 +25,7 @@ export const AddressTabConfig =  [
         "number" :"(708) 679-1990",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex":"(708) 679-1910",
+        "fax":"(708) 679-1910",
         "time" : " 24 hours, 7 days a week"
     },{
         "id" : "aurora",
@@ -34,7 +34,7 @@ export const AddressTabConfig =  [
         "number" :"(630) 236-0893",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " Mon: Closed <br>   Tuesday - Friday : 10 am to 7pm<br>   Saturday : 8 am to 6pm<br>       Sunday : 12 am to 7pm"
     },{
         "id" : "95",
@@ -43,25 +43,16 @@ export const AddressTabConfig =  [
         "number" :"(708) 474-4522",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " 24 hours, 7 days a week"
     },{
-        "id" : "lansing",
-        "name" : "Lansing",
-        "address" : " 2 River Place Lansing, IL 60438",
-        "number" :"(708) 474-4522",
-        "email" : "menashairdesign.enquiry@gmail.com",
-        "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
-        "time" : " Mon: Closed<br>Tuesday - Friday : 10 am to 7pm<br> Saturday : 8 am to 6pm<br>  Sunday : 12 am to 7pm"
-    },{
         "id" : "merr",
-        "name" : "Lansing",
+        "name" : "Merrillville",
         "address" : "7706 Broadway Merrillville Indiana 46410 ",
         "number" :"1 219-750-9079",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " Now Open<br>     Mon - Fri: 09:00 am to 08:00 pm<br>     Sat: 08:00 am to 07:00 pm<br>      Sun: 09:00 am to 06:00 pm"
     },{
         "id" : "35",
@@ -70,7 +61,7 @@ export const AddressTabConfig =  [
         "number" :"(312) 225-6333",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(312) 225-6313",
+        "fax" : "(312) 225-6313",
         "time" : " 24 hours, 7 days a week"
     },{
         "id" : "marsh",
@@ -79,7 +70,7 @@ export const AddressTabConfig =  [
         "number" :"773-568-5156",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " Mon - Fri: 09:00 am to 08:00 pm<br    Sat: 08:00 am to 07:00 pm<br>    Sun: 09:00 am to 06:00 pm"
     },{
         "id" : "forest",
@@ -88,7 +79,7 @@ export const AddressTabConfig =  [
         "number" :"708-689-8392",
         "email" : "menashairdesign.enquiry@gmail.com",
         "web" : "http://www.menahairdesign.com",
-        "fex" : "(708) 679-1910",
+        "fax" : "(708) 679-1910",
         "time" : " Now Open<br>     Mon - Fri: 09:00 am to 08:00 pm<br>     Sat: 08:00 am to 07:00 pm<br>Sun: 09:00 am to 06:00 pm"
     }
 ]
